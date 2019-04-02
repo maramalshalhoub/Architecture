22c49686931e0a594c982ae5a3be15df8d416b1d
maramalshalhoub <maram_alshalhoub@hotmail.com>
2019-04-02 14:51:53 +0300

    Slide Show Successful

--- src/Header.js
+++ src/Header.js
@@ -11,7 +11,12 @@ import {
   DropdownToggle,
   DropdownMenu,
   DropdownItem } from 'reactstrap';
-  import Services from './Services';
+import { Container, Row, Col } from 'reactstrap';
+import Card from 'react-bootstrap/Card'
+import CardGroup from 'react-bootstrap/CardGroup'
+import Projects from './Projects'
+import Services from './Services';
+import data from './data'
 
  class Header extends Component {
   constructor(props) {
@@ -35,12 +40,15 @@ import {
       isOpen: !this.state.isOpen
     });
   }
+
   render() {
     const change = (this.state.page)==='home'? ' is-active' : ' in-active';
+    console.log(this.state.details);
+    
     return (
 <div>
   <div>
-        <Navbar color="light" light expand="md">
+      <Navbar color="light" light expand="md">
           <NavbarBrand href="/home" padding="0" margin="0"><img src="https://i.ibb.co/DR9HHc4/logoNew.png" alt="logo"/></NavbarBrand>
           <NavbarToggler onClick={this.toggle} />
           <Collapse isOpen={this.state.isOpen} navbar>
@@ -51,22 +59,9 @@ import {
               <NavItem>
                 <NavLink href="/Projects/">Projects</NavLink>
               </NavItem>
-              <UncontrolledDropdown nav inNavbar>
-                <DropdownToggle nav caret>
-                Services
-                </DropdownToggle>
-                <DropdownMenu right>
-                  <DropdownItem onClick={()=>this.handleServices()}>
-                  Architecture
-                  </DropdownItem>
-                  <DropdownItem>
-                  Interior Design
-                  </DropdownItem>
-                  <DropdownItem>
-                  Project Management
-                  </DropdownItem>
-                </DropdownMenu>
-              </UncontrolledDropdown>
+              <NavItem>
+                <NavLink onClick={this.handleServices}>Services</NavLink>
+              </NavItem>
               <NavItem>
                 <NavLink href="/Portfolio/">Portfolio</NavLink>
               </NavItem>
@@ -75,12 +70,91 @@ import {
               </NavItem>
             </Nav>
           </Collapse>
-        </Navbar>
+      </Navbar>
   </div>
   {this.state.details}
   <div className={change}>
     <img src="https://i.ibb.co/TmnXYkz/image-2019-03-31.jpg" width="1280px"/>
   </div>
+  <div>
+  {/* {this.state.details} */}
+      <div className={change}>
+        <CardGroup>
+        <Card>
+        <Card.Img variant="top" src="https://i.ibb.co/jzz5dxZ/Architectural-Engineers.jpg" />
+          <Card.Body>
+            <Card.Title>About Us</Card.Title>
+            <Card.Text>
+                We are an architectural interior and Project management office,<br/> experienced in the Saudi market that select the best team of professionals<br/> and companies in order to achieve the goals of every project we develop.                
+            </Card.Text>
+          </Card.Body>
+        </Card>
+        <Card>
+          <Card.Img variant="top" src="https://i.ibb.co/jzz5dxZ/Architectural-Engineers.jpg" />
+          <Card.Body>
+            <Card.Title>Projects</Card.Title>
+            <Card.Text>
+                 We are an architectural interior and Project management office, experienced in the 
+            </Card.Text>
+          </Card.Body>
+          <Card.Footer>
+          <small className="text-muted"> <Projects  /><a href="Projects.js"> Read more..</a></small>
+          </Card.Footer>
+        </Card>
+        <Card>
+        <Card.Img variant="top" src="https://i.ibb.co/jzz5dxZ/Architectural-Engineers.jpg" />
+          <Card.Body>
+            <Card.Title onClick={this.handleServices} >Services</Card.Title>
+            <Card.Text>
+            We are an architectural interior and Project management office, experienced in the 
+            </Card.Text>
+          </Card.Body>
+          <Card.Footer>
+            <small className="text-muted" > Read more..</small>
+          </Card.Footer>
+        </Card>
+      </CardGroup>
+      <div>
+      <h4>Some of Our Clients</h4>
+      <Container>
+        <Row>
+        <Col>
+            <Card>
+            <Card.Img variant="top" src="https://i.ibb.co/yBdXqN1/d6e87ffd-fb4f-400e-a0e9-6504391e55d3-16x9-1200x676.jpg" height="195px" width="200px"/>
+            </Card>
+        </Col>
+        <Col>
+            <Card>
+            <Card.Img variant="top" src="https://i.ibb.co/tKd6Ksq/RUlbh-XYL-400x400.jpg" height="195px" width="200px"/>
+            </Card>
+        </Col>
+        <Col>
+            <Card>
+            <Card.Img variant="top" src="https://i.ibb.co/YXGy7WQ/Only-Roses-square-logo-white-on-black.png" height="195px" />
+            </Card>
+        </Col>
+        </Row>
+        <Row>
+        <Col>
+            <Card>
+            <Card.Img variant="top" src="https://i.ibb.co/y6hZpJ2/DOC8-G56-WAAIxe-WT.jpg" height="195px" />
+            </Card>
+        </Col>
+        <Col>
+            <Card>
+            <Card.Img variant="top" src="https://i.ibb.co/mqqNhKs/Tgkw-Bm3o-400x400.jpg" height="195px"/>
+            </Card>
+        </Col>
+        <Col>
+            <Card>
+            <Card.Img variant="top" src="https://i.ibb.co/k8Ct9HP/images-2.jpg" height="195px" />
+            </Card>
+        </Col>
+        </Row>
+    </Container>
+    </div>
+    </div>
+    </div>
 </div>
     )
   }
--- src/Services.js
+++ src/Services.js
@@ -1,68 +1,109 @@
 import React, { Component } from 'react';
 import data from './data'
 import Card from 'react-bootstrap/Card';
-
+import Carousel from 'react-bootstrap/Carousel'
 export default class Services extends Component {
-    constructor(props){
-        super(props);
-        this.state ={
-            properties: data.properties,
-            // made first one only active
-            property:data.properties[0]
-        }
-    }
-// Method worked only when on click on button
-// These methods resets the props state 
-    nextProperty = () =>{
-        const nextIndex=this.state.property.index+1;
-        this.setState({property:data.properties[nextIndex]})
-    }
-    prevProperty = () =>{
-        const nextIndex=this.state.property.index-1;
-        this.setState({property:data.properties[nextIndex]})
-    }
+//     constructor(props){
+//         super(props);
+//         this.state ={
+//             properties: data.properties,
+//             // made first one only active
+//             property:data.properties[0]
+//         }
+//     }
+// // Method worked only when on click on button
+// // These methods resets the props state 
+//     nextProperty = () =>{
+//         const nextIndex = this.state.property.index+1;
+//         this.setState({property:data.properties[nextIndex]})
+//     }
+//     prevProperty = () =>{
+//         const nextIndex=this.state.property.index-1;
+//         this.setState({property:data.properties[nextIndex]})
+//     }
 
+//   render() {
+//     const test= this.state.properties.map(property => {
+//         return <div id={`card-${this.state.property.index}`} className="img">
+//                  <img src={property.picture} />
+//                  </div>
+//   })
+//    console.log(this.state.properties[0].picture);
+//       return (
+//     <div>
+//         <button  onClick={()=>this.nextProperty()}
+//             disabled={this.state.property.index===data.properties.length-1}>
+//            Next
+//         </button>
+//         <button  onClick={()=>this.nextProperty()}
+//             disabled={this.state.property.index===0}>
+//            Previous
+//         </button>
 
+//         <div className="page">
+//             <section>
+//                 <h1>Services Accomplished</h1>
+//             </section>
+//         {/* Render and align everything in  the middle */}
+//             <div className={`cards-slider active-slide-${this.state.property.index}`}>
+//     {/* To stretch all the cards next to each other
+//     Created inline style that calculates the transfom  
+//     to move all cards to the exact off set calculated  */}
+//     {/* style={{'transform':`translateX(-${this.state.property.index*(100/this.state.properties.length)}%)`}} */}
+//                 <div className="cards-slider-wrapper">
+//                     {test}
+//                 </div>
+                 
+//             </div>
+//         </div>
+// </div>
+// )
+// }
+// }
+constructor(props, context) {
+    super(props, context);
+
+    this.handleSelect = this.handleSelect.bind(this);
+
+    this.state = {
+      index: 0,
+      direction: null,
+    };
+  }
+
+  handleSelect(selectedIndex, e) {
+    this.setState({
+      index: selectedIndex,
+      direction: e.direction,
+    });
+  }
 
   render() {
-    //   const l= property.index;
-      const {properties, property} = this.state;
 
-      return (
-
-<div>
-        <button  onClick={()=>this.nextProperty()}
-            disabled={property.index===data.properties.length-1}>
-           Next
-        </button>
-        <button  onClick={()=>this.nextProperty()}
-            // disabled={l===0}
-            >
-           Previous
-        </button>
-
-    <div className="page">
-        <section>
-           
-            <h1>Slide Show Services </h1>
-        </section>
-{/* Render and align everything in  the middle */}
-        <div className={`cards-slider active-slide-${property.index}`}>
-{/* To stretch all the cards next to each other
-Created inline style that calculates the transfom  
-to move all cards to the exact off set calculated  */}
-        <div className="cards-slider-wrapper" style={{
-            'transform':`translateX(-${property.index*(100/properties.length)}%)`}}>
-            {
-             properties.map(property => 
-             <Card key="property.id" property={property} />)
-            }
-        </div> 
-        </div>
-    </div>
-</div>
+    let carousels = data.properties.map((item, index)=>{
+return (
+    <Carousel.Item key={index}>
+    <img
+      className="d-block w-100"
+      src={item.picture}
+      
+    />
+  </Carousel.Item>
 )
-  }
-}
+    })
 
+    const { index, direction } = this.state;
 
+    return (
+<div>
+<Carousel   
+        // activeIndex={index}
+        // direction={direction}
+        // onSelect={this.handleSelect}
+        >
+ 
+  {carousels}
+</Carousel>
+</div>
+    )}}
+    // render(<ControlledCarousel />);
