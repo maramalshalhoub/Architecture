2e9bcb628e1de95448832ef7c63cd41958184012
maramalshalhoub <maram_alshalhoub@hotmail.com>
2019-04-03 16:19:44 +0300

    Updates

---
+++ precache-manifest.f5b8b78d17906a7122564aa45c09e6d9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "eb2b7096e2eae658d03c",
+    "url": "/Architecture/static/js/runtime~main.78604aab.js"
+  },
+  {
+    "revision": "e5b0b3683b7c1436f8e4",
+    "url": "/Architecture/static/js/main.4740f610.chunk.js"
+  },
+  {
+    "revision": "b1e67b4d9a648a9c8098",
+    "url": "/Architecture/static/js/2.a08a9e85.chunk.js"
+  },
+  {
+    "revision": "e5b0b3683b7c1436f8e4",
+    "url": "/Architecture/static/css/main.362f7b14.chunk.css"
+  },
+  {
+    "revision": "b1e67b4d9a648a9c8098",
+    "url": "/Architecture/static/css/2.dd0f1e91.chunk.css"
+  },
+  {
+    "revision": "d2fba96b62bbba3bd427eaca453e8880",
+    "url": "/Architecture/index.html"
+  }
+];
